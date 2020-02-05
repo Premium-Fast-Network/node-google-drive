3a03b8fb8a3ef8b9452ddff0debfabf50ab8f42a
JuniYadi <juniyadi94@gmail.com>
2020-02-05 16:23:08 +0700

    Added Credentials Sample

---
+++ example/credentials.sample.js
@@ -0,0 +1,10 @@
+const credentials = {
+    clientId: 'YOUR-API-CLIENT-ID',
+    clientSecret: 'YOUR-API-CLIENT-SECRET',
+    scopes: [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file',
+    ]
+}
+
+module.exports = credentials
