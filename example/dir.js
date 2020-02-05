74c7f3491157fd401a326017bdefd0224a2925c7
JuniYadi <juniyadi94@gmail.com>
2020-02-06 00:59:00 +0700

    Added Example Function `createDir` and  Delete a Folder/Dir

---
+++ example/dir.js
@@ -0,0 +1,25 @@
+const GoogleDrive = require('../src/googledrive')
+const credentials = require('./credentials')
+
+const gd = new GoogleDrive({
+    clientId: credentials.clientId,
+    clientSecret: credentials.clientSecret,
+    scopes: credentials.scopes
+})
+
+// set user token
+const setToken = gd.setToken(credentials.user)
+
+// create new folders
+// id folder parents inside array
+// gd.createDir('NAME-FOLDER', ['FOLDER-ID-PARENTS'])
+
+// delete a folder
+// can use function deleteFiles
+gd.deleteFiles('FOLDER-ID')
+.then((res) => {
+    console.log(res)
+})
+.catch((err) => {
+    console.log(err)
+})
