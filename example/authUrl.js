3e64e1be0b12f5073e30cba132f36f24f1653883
JuniYadi <juniyadi94@gmail.com>
2020-02-05 16:23:27 +0700

    Update Example authUrl using Credential

--- example/authUrl.js
+++ example/authUrl.js
@@ -1,18 +1,10 @@
 const GoogleDrive = require('../src/googledrive')
+const credentials = require('./credentials')
 
 const gd = new GoogleDrive({
-    clientId: '777823450815-pe11i6mugqsk8n1c2mevglhgoaoge92l.apps.googleusercontent.com',
-    clientSecret: 'phz92mGe-oyMayNOYf4a3vmA',
-    scopes: [
-        'https://www.googleapis.com/auth/userinfo.profile',
-        'email',
-        'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/drive.metadata',
-        'https://www.googleapis.com/auth/drive.metadata.readonly',
-        'https://www.googleapis.com/auth/drive.appdata',
-        'https://www.googleapis.com/auth/drive.file',
-        'https://www.googleapis.com/auth/drive.photos.readonly'
-    ],
+    clientId: credentials.clientId,
+    clientSecret: credentials.clientSecret,
+    scopes: credentials.scopes,
     redirect: 'http://localhost/oauth' // delete this object if you want to get token only after login or authorize
 })
 
