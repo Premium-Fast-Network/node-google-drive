e454dc2dd45b8594f1168945083152d4202d244a
JuniYadi <juniyadi94@gmail.com>
2020-02-05 16:46:42 +0700

    Added Example authCode

---
+++ example/authCode.js
@@ -0,0 +1,16 @@
+const GoogleDrive = require('../src/googledrive')
+const credentials = require('./credentials')
+
+const gd = new GoogleDrive({
+    clientId: credentials.clientId,
+    clientSecret: credentials.clientSecret,
+    scopes: credentials.scopes
+})
+
+gd.verifyToken('AUTH-CODE-HERE')
+.then((res) => {
+    console.log(res.tokens)
+})
+.catch((err) => {
+    console.log(err)
+})
