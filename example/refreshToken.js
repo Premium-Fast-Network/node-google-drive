20f4729f051a7b5d7be86c3064e9cb620b68fffb
JuniYadi <juniyadi94@gmail.com>
2020-02-05 19:53:10 +0700

    Added Example Refresh Token

---
+++ example/refreshToken.js
@@ -0,0 +1,23 @@
+const GoogleDrive = require('../src/googledrive')
+const credentials = require('./credentials')
+
+const gd = new GoogleDrive({
+    clientId: credentials.clientId,
+    clientSecret: credentials.clientSecret,
+    scopes: credentials.scopes
+})
+
+// set user token
+const setToken = gd.setToken(credentials.user)
+
+// refresh token manual
+const refresh = gd.refreshToken()
+
+refresh.then((res) => {
+    // you're function for save new token to database to database
+
+    console.log(res.data)
+})
+.catch((err) => {
+    console.log(err)
+})
