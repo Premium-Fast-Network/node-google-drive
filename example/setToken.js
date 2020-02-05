acbbb1f448ce5fe1250c1a636a18a670df09fa9c
JuniYadi <juniyadi94@gmail.com>
2020-02-05 19:53:02 +0700

    Added Example Set and Get Token

---
+++ example/setToken.js
@@ -0,0 +1,18 @@
+const GoogleDrive = require('../src/googledrive')
+const credentials = require('./credentials')
+
+const gd = new GoogleDrive({
+    clientId: credentials.clientId,
+    clientSecret: credentials.clientSecret,
+    scopes: credentials.scopes
+})
+
+// set user token
+const setToken = gd.setToken(credentials.user)
+console.log('set user token')
+console.log(setToken)
+
+// get user token
+const getToken = gd.getToken()
+console.log('get user token')
+console.log(getToken)
