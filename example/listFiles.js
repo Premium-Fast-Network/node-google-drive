a495bf78d0fed1347c89c16e92e63c5208e5e790
JuniYadi <juniyadi94@gmail.com>
2020-02-05 22:28:39 +0700

    Added Example `listFiles.js`

---
+++ example/listFiles.js
@@ -0,0 +1,29 @@
+const GoogleDrive = require('../src/googledrive')
+const credentials = require('./credentials')
+
+const gd = new GoogleDrive({
+    clientId: credentials.clientId,
+    clientSecret: credentials.clientSecret,
+    scopes: credentials.scopes
+})
+
+// set user token
+const setToken = gd.setToken(credentials.user)
+
+// if you want custom query, just put object in function
+// example : https://developers.google.com/drive/api/v3/reference/files/list
+
+// with query
+// gd.listFiles(query)
+
+// without query
+gd.listFiles()
+.then((res) => {
+    return res.data
+})
+.then((res) => {
+    console.log(res)
+})
+.catch((err) => {
+    console.log(err)
+})
